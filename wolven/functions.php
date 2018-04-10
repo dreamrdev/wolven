fa312d1ddabc3b0fcbe91ef389fb97df6c9d6a88
dreamrdev <34534521+dreamrdev@users.noreply.github.com>
2018-04-10 00:56:38 -0400

    Add files via upload

--- wolven/functions.php
+++ wolven/functions.php
@@ -303,4 +303,7 @@ function pagination_bar() {
 		));
 	}
 }
-
+function new_excerpt_more($more) {
+	return ' <a class="read-more" href="' . get_permalink( get_the_ID() ) . '">Read More</a>';
+}
+add_filter('excerpt_more', 'new_excerpt_more');
